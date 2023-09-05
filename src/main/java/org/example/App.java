5a59802b69e7c7cd9dcb8203a5c69b19c621564a
maxer <max.erkmar@iths.se>
2023-09-05 15:16:45 +0200

    updates

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,74 +1,50 @@
 package org.example;
 
-import java.util.NoSuchElementException;
+
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
         int[] priceStorage = new int[24];
-        String input = "";
-        while (!exit) {
-            String menu = """
+        while (true) {
+            printMenu();
+            String input = scanner.nextLine();
+            if (input.equals("e") || input.equals("E")) break;
+            else if (input.equals("1")) priceStorage = getPrices(scanner);
+            else if (input.equals("2")) getMinMaxMean(priceStorage);
+            else if (input.equals("3")) System.out.println("3");
+            else if (input.equals("4")) System.out.println("4");
+            else System.out.println("Felaktig inmatning!");
+            scanner.useDelimiter("\n");
+        }
+    }
+    private static void printMenu(){
+        String menu = """
                     Elpriser
                     ========
-                    1. Inmatnin
+                    1. Inmatning
                     2. Min, Max och Medel
                     3. Sortera
                     4. Bästa Laddningstid (4h)
                     e. Avsluta
                     """;
-            System.out.println(menu);
-            Scanner scanner = new Scanner(System.in);
-            boolean inputNext = scanner.hasNext();
-            if (inputNext) input = scanner.next();
-            //int[] temp = {100, 10, 1, 10, 10, 10, 210, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-
-            switch (input) {
-                case "1" -> priceStorage = set_prices();
-                case "2" -> {
-                    int[] minPrice = min_price(priceStorage);
-                    int[] maxPrice = max_price(priceStorage);
-                    double avgPrice = avg_price(priceStorage);
-                    int minTime = minPrice[1];
-                    int maxTime = maxPrice[1];
-                    String response = String.format("""
-                            Lägsta pris: %02d-%02d, %d öre/kWh
-                            Högsta pris: %02d-%02d, %d öre/kWh
-                            Medelpris: %.2f öre/kWh
-                            """, minTime, minTime + 1, minPrice[0], maxTime, maxTime + 1, maxPrice[0], avgPrice);
-                    System.out.println(response);
-                }
-                case "3" -> System.out.println("Sortera: ");
-                case "4" -> System.out.println("Bästa Laddningstid: ");
-                case "e", "E" -> {
-                    System.out.println("Avslutar");
-                    exit = true;
-                }
-                default -> System.out.println("fel");
-            }
-
-        }
+        System.out.println(menu);
     }
-    public static int[] set_prices() {
-        System.out.println("Mata in värden: ");
-        Scanner scanner = new Scanner(System.in);
-        int[] prices = new int[24];
 
-        try {
-            for (int i = 0; i < prices.length; i++) {
-                System.out.println(i + ": ");
-                int inputPrice = scanner.nextInt();
-                prices[i] = inputPrice;
-            }
-        } catch (NoSuchElementException elementException) {
-            System.out.println("Felaktigt tecken! ");
+    public static int[] getPrices(Scanner sc) {
+        System.out.println("Mata in värden: ");
+        int[] prices = new int[24];
+        for (int i = 0; i < prices.length; i++) {
+            String input = sc.nextLine();
+            sc.useDelimiter("\n");
+            prices[i] = Integer.parseInt(input);
         }
         return prices;
     }
 
-    private static int[] min_price(int[] prices) {
+    private static int[] getMinPrice(int[] prices) {
         int[] minArray = new int[2];
         minArray[0] = prices[0];
         for (int i = 0; i < prices.length; i++) {
@@ -80,7 +56,7 @@ public class App {
         return minArray;
     }
 
-    private static int[] max_price(int[] prices) {
+    private static int[] getMaxPrice(int[] prices) {
         int[] maxArray = new int[2];
         maxArray[0] = prices[0];
         for (int i = 0; i < prices.length; i++) {
@@ -91,9 +67,23 @@ public class App {
         }
         return maxArray;
     }
-    private static double avg_price(int[] prices) {
+
+    private static double getAverage(int[] prices) {
         int sum = 0;
-        for (int i = 0; i < prices.length; i++) sum += prices[i];
+        for (int price : prices) {
+            sum = sum + price;
+        }
         return (double) sum / prices.length;
+    }
+    private static void getMinMaxMean(int[] prices) {
+        int[] minPrice = getMinPrice(prices);
+        int[] maxPrice = getMaxPrice(prices);
+        double avgPrice = getAverage(prices);
+        String minMaxMean = String.format("""
+            Lägsta pris: %02d-%02d, %d öre/kWh
+            Högsta pris: %02d-%02d, %d öre/kWh
+            Medelpris: %.2f öre/kWh
+            """, minPrice[1], minPrice[1]+1, minPrice[0], maxPrice[1], maxPrice[1]+1, maxPrice[0], avgPrice);
+        System.out.println(minMaxMean);
     }
 }
